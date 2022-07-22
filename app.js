8b7443b6141bee7e4fc70d0db160c9960e185afe
Jyotirani12 <jyoti.lohan@gmail.com>
2022-07-22 23:23:46 +0530

    first

--- app.js
+++ app.js
@@ -2,27 +2,24 @@ let express=require('express');
 let app=express();
 let port=process.env.port||9870;
 let dotenv=require('dotenv');
+let morgan=require('morgan');
+let bodyParser=require('body-parser');
+let cors=require('cors');
 dotenv.config();
 let mongo=require('mongodb');
 let MongoClient=mongo.MongoClient;
 //let mongoUrl="mongodb://localhost:27017";
-let mongoUrl="mongodb+srv://test:test123@cluster0.cxbxf.mongodb.net/?retryWrites=true&w=majority";
+let mongoUrl="mongodb+srv://test:test123@cluster0.cxbxf.mongodb.net/?retryWrites=true&w=majority"
 let db;
+app.use(morgan('common'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(cors());
 // app.get('/loc',(res,res))
 app.get('/',(req,res)=>{
     res.send("hi from express3");
 })
-//app.listen(port)
-// app.listen(port,()=>{
-//     console.log(`listen on port ${port}`);
-// })
-// app.get('/category',(req,res)=>{
-//     db.collection('category').find().toArray((err,result)=>{
-//         if(err) throw err;
-//         res.send(result);
-//     })
-   
-// })
+
 app.get('/category',(req,res) => {
     db.collection('category').find().toArray((err,result) => {
         if(err) throw err;
@@ -37,6 +34,14 @@ app.get('/subcategory',(req,res) => {
 })
 //to show all Product
 app.get('/product',(req,res)=>{
+    db.collection('product').find({},{'product_name':1,'_id':0}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+})
+
+app.get('/details/:id',(req,res)=>{
+    let id = Number(req.params.id);
     db.collection('product').find().toArray((err,result)=>{
         if(err) throw err;
         res.send(result);
@@ -45,8 +50,21 @@ app.get('/product',(req,res)=>{
 //To show Product Acc to category
 app.get('/product/:id',(req,res)=>{
     let id=req.params.id;
-
     db.collection('product').find({category_id:Number(id)}).toArray((err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    })
+})
+////To show Product Acc to Brands
+app.get('/products',(req,res)=>{
+    let query = {}
+    let brandId = Number(req.query.brandId);
+    if(brandId){
+        query = {"Brand.brand_id":brandId}
+    }else{
+        query = {}
+    }
+    db.collection('product').find(query).toArray((err,result)=>{
         if(err) throw err;
         res.send(result);
     })
@@ -59,7 +77,7 @@ app.get('/product/:id',(req,res)=>{
 //         res.send(result);
 //     })
 // })
-
+// app.get('filter'/)
 
 MongoClient.connect(mongoUrl,(err,client) =>{
     if(err) console.log(`Error While Connecting`);
