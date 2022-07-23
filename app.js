a8963ad2c2bec7a1e7dbf3ce714127d5838cde8f
Jyoti <104449403+Jyotirani12@users.noreply.github.com>
2022-07-23 21:49:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ let cors=require('cors');
 dotenv.config();
 let mongo=require('mongodb');
 let MongoClient=mongo.MongoClient;
-let MongoUrl="mongodb://localhost:27017";
-//let MongoUrl="mongodb+srv://test:test123@cluster0.cxbxf.mongodb.net/?retryWrites=true&w=majority"
+//let MongoUrl="mongodb://localhost:27017";
+let MongoUrl="mongodb+srv://test:test123@cluster0.cxbxf.mongodb.net/?retryWrites=true&w=majority"
 let db;
 app.use(morgan('common'));
 app.use(bodyParser.urlencoded({extended:true}));
