77e07ca7835d293582ff416cc61df16540c7e859
Jyotirani12 <jyoti.lohan@gmail.com>
2022-07-23 13:28:12 +0530

    first

--- app.js
+++ app.js
@@ -20,71 +20,74 @@ app.get('/',(req,res)=>{
     res.send("hi from express3");
 })
 
-app.get('/category',(req,res) => {
-    db.collection('category').find().toArray((err,result) => {
-        if(err) throw err;
-        res.send(result);
-    })
-})
-app.get('/subcategory',(req,res) => {
-    db.collection('subcategory').find().toArray((err,result) => {
-        if(err) throw err;
-        res.send(result);
-    })
-})
-//to show all Product
-app.get('/product',(req,res)=>{
-    db.collection('product').find({},{'product_name':1,'_id':0}).toArray((err,result)=>{
-        if(err) throw err;
-        res.send(result);
-    })
-})
-
-app.get('/details/:id',(req,res)=>{
-    let id = Number(req.params.id);
-    db.collection('product').find().toArray((err,result)=>{
-        if(err) throw err;
-        res.send(result);
-    })
-})
-//To show Product Acc to category
-app.get('/product/:id',(req,res)=>{
-    let id=req.params.id;
-    db.collection('product').find({category_id:Number(id)}).toArray((err,result)=>{
-        if(err) throw err;
-        res.send(result);
-    })
-})
-////To show Product Acc to Brands
-app.get('/products',(req,res)=>{
-    let query = {}
-    let brandId = Number(req.query.brandId);
-    if(brandId){
-        query = {"Brand.brand_id":brandId}
-    }else{
-        query = {}
-    }
-    db.collection('product').find(query).toArray((err,result)=>{
-        if(err) throw err;
-        res.send(result);
-    })
-})
+// app.get('/category',(req,res) => {
+//     db.collection('category').find().toArray((err,result) => {
+//         if(err) throw err;
+//         res.send(result);
+//     })
+// })
+// app.get('/subcategory',(req,res) => {
+//     db.collection('subcategory').find().toArray((err,result) => {
+//         if(err) throw err;
+//         res.send(result);
+//     })
+// })
+// //to show all Product
 // app.get('/product',(req,res)=>{
-//    let id=req.query.category_id;
-// //let {category_id}=req.query;
+//     db.collection('product').find({},{'product_name':1,'_id':0}).toArray((err,result)=>{
+//         if(err) throw err;
+//         res.send(result);
+//     })
+// })
+
+// app.get('/details/:id',(req,res)=>{
+//     let id = Number(req.params.id);
+//     db.collection('product').find().toArray((err,result)=>{
+//         if(err) throw err;
+//         res.send(result);
+//     })
+// })
+// //To show Product Acc to category
+// app.get('/product/:id',(req,res)=>{
+//     let id=req.params.id;
 //     db.collection('product').find({category_id:Number(id)}).toArray((err,result)=>{
 //         if(err) throw err;
 //         res.send(result);
 //     })
 // })
-// app.get('filter'/)
+// ////To show Product Acc to Brands
+// app.get('/products',(req,res)=>{
+//     let query = {}
+//     let brandId = Number(req.query.brandId);
+//     if(brandId){
+//         query = {"Brand.brand_id":brandId}
+//     }else{
+//         query = {}
+//     }
+//     db.collection('product').find(query).toArray((err,result)=>{
+//         if(err) throw err;
+//         res.send(result);
+//     })
+// })
+// // app.get('/product',(req,res)=>{
+// //    let id=req.query.category_id;
+// // //let {category_id}=req.query;
+// //     db.collection('product').find({category_id:Number(id)}).toArray((err,result)=>{
+// //         if(err) throw err;
+// //         res.send(result);
+// //     })
+// // })
+// // app.get('filter'/)
 
-MongoClient.connect(MongoUrl,(err,client) =>{
-    if(err) console.log(`Error While Connecting`);
-    db = client.db('amazon');
-       app.listen(port,() => {
-        console.log(`listening on port ${port}`)
+// MongoClient.connect(MongoUrl,(err,client) =>{
+//     if(err) console.log(`Error While Connecting`);
+//     db = client.db('amazon');
+//        app.listen(port,() => {
+//         console.log(`listening on port ${port}`)
 
-    })
-})
-
+//     })
+// })
+app.listen(port,() => {
+           console.log(`listening on port ${port}`)
+    
+        })
